2380c58dbf54d42466ae9ebfc9ef0d71264ef521
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2022-09-07 13:47:39 +0100

    first test of api resporse

--- tests/students/test_courses_api.py
+++ tests/students/test_courses_api.py
@@ -1,2 +1,18 @@
+import pytest
+from rest_framework.test import APIClient
+
+
 def test_example():
-    assert False, "Just test example"
+    assert 2 == 2
+
+
+@pytest.mark.django_db
+def test_api():
+    # Arrange
+    client = APIClient()
+
+    # Act
+    response = client.get('/courses/')
+
+    # Assert
+    assert response.status_code == 200
